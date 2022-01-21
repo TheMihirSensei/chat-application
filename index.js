523a527278d467c20fb2f47765f6d156dbea0c35
MihirSensei <mihirsidhdhapura41@gmail.com>
2022-01-21 10:16:13 +0530

    chat allmost done

--- index.js
+++ index.js
@@ -62,10 +62,39 @@ app.post('/user', async (req, res, next) => {
 	res.status(200).json({ message: "user added successfully", user: savedUser })
 })
 
+
+app.get("/users", async (req, res) => {
+	try{
+		let userData = await userModel.find().sort({user_name: 1})
+		if(userData.length >0){
+			res.status(200).json({messaeg:"success", data: userData})
+		}else{
+			res.status(404).json({message:"no data found"})
+		}
+	}catch(err){
+		res.status(500).json({message:"internal server Error", error: err?.message})
+	}
+})
+
+app.get("/history", async (req, res) => {
+	try{
+		const senderId = req.query.senderId
+		const receiverId = req.query.receiverId
+		let historyData = await chatModel.find({$or: [{senderId: senderId, receiverId: receiverId}, {senderId: receiverId, receiverId: senderId}]}).sort({createdAt: 1})
+		if(historyData.length > 0){
+			res.status(200).json({data: historyData})
+		}else{
+			res.status(404).json({message: "no history data found"})
+		}
+	}catch(err){
+		res.status(500).json({message:"Internal Server Error", error: err?.messsage})
+	}
+
+})
 const port = 5012
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/otmChat", { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+mongoose.connect("mongodb+srv://mihir:mihir@chat-cluster.cs3bs.mongodb.net/chatApp?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
 	if (err) {
 		console.log("issue is: ", err)
 	} else {
--- public/js/main.js
+++ public/js/main.js
@@ -1,58 +1,147 @@
-const socket = io()
+const socket = io();
 const chatContainer = document.getElementById("chat-container");
 const btnSubmit = document.getElementById("btnSubmit");
 const domUser = document.getElementById("users");
 const sendMsg = document.getElementById("msg");
 
+const addElem = (senderName, msg, date, cssClass) => {
+	let divMsg = document.createElement("div");
+	divMsg.classList.add("message");
+	divMsg.classList.add(cssClass);
+	divMsg.innerHTML = `<p class="meta">${senderName} <span>${date || new Date().toLocaleTimeString()
+		}</span></p>
+							<p class="text">
+								${msg}
+							</p>`;
+
+	chatContainer.appendChild(divMsg);
+};
+
+// add the users to the side panel
+const addUserDom = (users) => {
+	const domUser = document.getElementById("users");
+	let innerText = ''
+
+	users.forEach((user) => {
+		innerText +=
+			"<li class='user' onclick='chatWithUser(event)' id=" +
+			user._id +
+			" value=" +
+			user._id +
+			">" +
+			user.user_name +
+			"</li>";
+	});
+
+	domUser.innerHTML = innerText
+};
+
+const chatWithUser = (e) => {
+	localStorage.setItem("receiverId", e.target.id);
+	let userId = localStorage.getItem("userId")
+	const userLabel = JSON.parse(localStorage.getItem("activeUser")).filter(
+		(user) => user._id === e.target.id
+	)[0].user_name;
+	const domUserLabel = document.getElementById("userLabel");
+	const domUser = document.getElementById(e.target.id);
+	//remove active class from the others...
+	const activeClassEle = document.getElementsByClassName("active");
+	for (const item of activeClassEle) {
+		item.classList.remove("active");
+	}
+	domUser.classList.add("active");
+	domUserLabel.innerHTML = userLabel;
+
+	//get history of receiver and sender and change the dom according to it. 
+	fetch(`http://localhost:5012/history?senderId=${userId}&receiverId=${e.target.id}`)
+		.then(data => data.json())
+		.then(res => {
+			if (res.data.length > 0) {
+				let receiverId = localStorage.getItem("receiverId")
+				console.log("ohk history data: ", res.data)	
+				chatContainer.innerHTML = ""
+				res.data.forEach(chat => {
+					let receiverName = JSON?.parse(localStorage.getItem("activeUser"))?.filter(
+						(user) => user._id === receiverId
+					)[0].user_name;
+					let userName = localStorage.getItem("userName")
+					let name = chat.senderId === userId ? userName : receiverName;
+					let cssClass = chat.senderId === userId ? "right" : "left";
+					console.log("name si: ", name, "user calas alls", cssClass)
+
+					addElem(name, chat.msg, chat.createdAt, cssClass);
+
+				})
+			} else {
+				chatContainer.innerHTML = ""
+			}
+
+		})
+};
 
 
 (() => {
-	let userId = localStorage.getItem("userId")
-	let receiverId = localStorage.getItem("receiverId")
-	let userName = localStorage.getItem("userName")
-	let receiverName = JSON.parse(localStorage.getItem("activeUser")).filter(user => user._id === receiverId)[0].user_name
-	socket.userId = localStorage.getItem("userId")
+	let userId = localStorage.getItem("userId");
+	let receiverId = localStorage.getItem("receiverId");
+	let userName = localStorage.getItem("userName");
+	socket.userId = localStorage.getItem("userId");
 
 	//first user arrive to chat
-	socket.emit("newUser", { userId: localStorage.getItem("userId"), userName: localStorage.getItem("userName") })
-	//for getting user all the history with specific user
-	socket.on("history", (data) => {
-		console.log(" all history of msg, ", data)
 
-		let actualHistory = data.filter(chat => {
-			return (chat.senderId === userId && chat.receiverId === receiverId) || (chat.senderId === receiverId && chat.receiverId === userId)
-		})
+	let userWith = document.getElementById("userWith")
+	userWith.innerHTML += ` ${userName}`
 
-		actualHistory.forEach(chat => {
-			let name = (chat.senderId === userId) ? userName : receiverName
-			let cssClass = (chat.senderId === userId) ? "right" : "left"
-			addElem(name, chat.msg, chat.createdAt, cssClass)
-		})
-		console.log("history of msg, ", actualHistory)
-	})
+	socket.emit("newUser", {
+		userId: localStorage.getItem("userId"),
+		userName: localStorage.getItem("userName"),
+	});
+	if (!receiverId) {
+		console.log("did not able to receive:::");
+	} else {
 
-	let domUserEle = document.getElementById(receiverId)
-	domUserEle.classList.add('active')
-	// const domUserLabel = document.getElementById("userLabel")
-	// domUserLabel.innerHTML = receiverName
+		fetch("http://localhost:5012/users")
+			.then((res) => res.json())
+			.then((data) => {
+				console.log("finally tdata is: ", data);
+				//add active users to local storage
+
+
+				let currentUser = data.data.filter((user) => user._id !== userId);
+				addUserDom(currentUser)
+
+				let receiverName = JSON?.parse(localStorage.getItem("activeUser"))?.filter(
+					(user) => user._id === receiverId
+				)[0].user_name;
+
+				//for getting user all the history with specific user
+				socket.on("history", (data) => {
+					let actualHistory = data.filter((chat) => {
+						return (
+							(chat.senderId === userId && chat.receiverId === receiverId) ||
+							(chat.senderId === receiverId && chat.receiverId === userId)
+						);
+					});
+
+					actualHistory.forEach((chat) => {
+						let name = chat.senderId === userId ? userName : receiverName;
+						let cssClass = chat.senderId === userId ? "right" : "left";
+						addElem(name, chat.msg, chat.createdAt, cssClass);
+					});
+				});
+				document.getElementById(receiverId).classList.add("active")
+				document.getElementById("userLabel").innerHTML = receiverName;
+			})
+			.catch((err) => {
+				console.log("soemthing went wrong", err);
+			});
+
+
+		// get all the active User and add to dom element
+	}
+
+	//get all the active the user and add to the userElement
 
 })();
-
-
-const addElem = (senderName, msg, date, cssClass) => {
-	let divMsg = document.createElement("div");
-	divMsg.classList.add("message")
-	divMsg.classList.add(cssClass)
-	divMsg.innerHTML = `<p class="meta">${senderName} <span>${date || new Date().toLocaleTimeString()}</span></p>
-							<p class="text">
-								${msg}
-							</p>`
-
-	chatContainer.appendChild(divMsg)
-}
-
-
-
 
 //will implement when user selected receiver and want to send the message
 // const createRoom = (receiverId) => {
@@ -64,58 +153,46 @@ const addElem = (senderName, msg, date, cssClass) => {
 socket.on("activeUserList", (data) => {
 	//add the active user in local storage and update dom elements
 
-	console.log("data is: ", data)
+	console.log("data is: ", data);
 
 	//userID, userName
-	let userID = localStorage.getItem("userId")
-	let currentUser = data.filter(user => user._id !== userID)
+	let userID = localStorage.getItem("userId");
+	let currentUser = data.filter((user) => user._id !== userID);
 
-	localStorage.setItem('activeUser', JSON.stringify(currentUser))
+	localStorage.setItem("activeUser", JSON.stringify(currentUser));
 	// console.log("setItem localstorag is called: ", localStorage.getItem("activeUser"))
-	let innerText = ""
-	currentUser.forEach(user => {
-		innerText += "<li class='user' onclick='chatWithUser(event)' id=" + user._id + " value=" + user._id + ">" + user.user_name + "</li>"
-	})
+	addUserDom(currentUser)
+	let receiverId = localStorage.getItem("receiverId")
+	if (receiverId) {
+		document.getElementById(receiverId).classList.add("active")
+	}
 
 
-	domUser.innerHTML = innerText
-})
+});
 
 socket.on("receiver msg", (data) => {
-	const userId = localStorage.getItem("userId")
+	const userId = localStorage.getItem("userId");
 	if (data.receiverId === userId) {
 		//dom add the message
-		let senderName = JSON.parse(localStorage.getItem("activeUser")).filter(user => user._id === data.senderId)[0].user_name
-		addElem(senderName, data.msg, "left")
-
+		let senderName = JSON.parse(localStorage.getItem("activeUser")).filter(
+			(user) => user._id === data.senderId
+		)[0].user_name;
+		addElem(senderName, data.msg, new Date().toLocaleTimeString(), "left");
 	}
-})
+});
 
 btnSubmit.addEventListener("click", (e) => {
-	e.preventDefault()
-	let msg = sendMsg.value
-	console.log("msg iss::::", msg)
-	let senderId = localStorage.getItem("userId")
-	let userName = localStorage.getItem("userName")
-	let receiverId = localStorage.getItem("receiverId")
-	socket.emit('new message', { senderId: senderId, receiverId: receiverId, msg: msg })
-	addElem(userName, msg, new Date().toLocaleTimeString(), "right")
-	sendMsg.value = ''
-
-
-})
-
-const chatWithUser = (e) => {
-	localStorage.setItem("receiverId", e.target.id)
-	const userLabel = JSON.parse(localStorage.getItem('activeUser')).filter(user => user._id === e.target.id)[0].user_name
-	const domUserLabel = document.getElementById("userLabel")
-	const domUser = document.getElementById(e.target.id)
-	//remove active class from the others... 
-	const activeClassEle = document.getElementsByClassName("active")
-	for (const item of activeClassEle) {
-		item.classList.remove("active")
-	}
-	domUser.classList.add('active')
-	domUserLabel.innerHTML = userLabel
-}
+	e.preventDefault();
+	let msg = sendMsg.value;
+	let senderId = localStorage.getItem("userId");
+	let userName = localStorage.getItem("userName");
+	let receiverId = localStorage.getItem("receiverId");
+	socket.emit("new message", {
+		senderId: senderId,
+		receiverId: receiverId,
+		msg: msg,
+	});
+	addElem(userName, msg, new Date().toLocaleTimeString(), "right");
+	sendMsg.value = "";
+});
 
